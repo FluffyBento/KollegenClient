1b7736aea56fccaaa6c180ecd66e9fe7b0a27acf
Kollegen Dev <dev@kollegen.local>
2026-08-21 21:29:58 +0200

    Release 1.6.9: Mod-Menü an Launcher-Design angleichen (blaue Markenfarbe, glatte TTF-Schrift, Vektor-Icons, Glow)

--- kollegen-mod/src/main/java/dev/kollegen/client/KollegenMod.java
+++ kollegen-mod/src/main/java/dev/kollegen/client/KollegenMod.java
@@ -26,7 +26,7 @@ import java.util.Set;
 public class KollegenMod implements ClientModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("kollegen-client");
     public static final String MOD_ID = "kollegen-client";
-    public static final String VERSION = "1.6.8";
+    public static final String VERSION = "1.6.9";
     public static final long DISCORD_CLIENT_ID = 1538588736718373034L;
 
     private static boolean shiftWasDown = false;
